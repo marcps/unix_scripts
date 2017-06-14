f25a8b749af8ccf970fcecc262f5279559836868
Gh0st <marc.ps.12@gmail.com>
2017-06-14 20:49:41 +0200

    change to monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#Copyright Marc Pascual i Solé, created on 17/07/2017 
+#This program changes the status mode of the desired wireless interface to either MONITOR or MANAGED mode
+#IMPORTANT: in order to keep the variables defined by this program, it needs to be executed with the SOURCE cmd
+#################################################################################
 echo
 echo "        ______________________________________________________"
 echo "       / ______  ______  ________  _____    ____  M           \"
@@ -11,25 +16,86 @@ echo " /________________________________________________R______/"
 echo
 sleep 1.5
 echo "Created by Marc Pascual i Solé. 14/6/2017. marc.ps.12@gmail.com"
+sleep 0.5
+echo "Last modification: " $(stat $HOME/Documents/unix_scripts/monitor.sh | grep Modify | gawk '{ FS = " " } {print $2}')
+
+echo -n "--->Choose your option [monitor/managed]: "
+read ans
+echo
 case $ans in
 	"monitor"|"mon")
-		echo "This will set the Network Card to MONITOR Mode"
+		echo "The Network Card will be set to MONITOR Mode"
 		sleep 0.6
-		echo -n "Are you sure you want to continue?[Y/n]: "
+		echo -n "[*]Are you sure you want to continue?[Y/n]: "
 		read yn
+		echo
+		sleep 0.8
 		case $yn in
-			"y"|"Y")
+			"Y"|"y"|"yes"|"si"|"ja"|"Hlja"|"hlja")
+
+				#Reads the user's input:
+                		echo -n "[*] INTERFACE name: "
+                		read int
+				echo
+				sleep 1
+				echo "[+] Stopping Network Manager"
 				service NetworkManager stop
-				ifconfig wlan0 down
-				iwconfig wlan0 mode monitor
-				ifconfig wlan0 up
-				echo "Checking for interfering processes:"
-				airmon-ng check wlan0
+				sleep 1
+			#Changes the Network Card Mode and changes MACAddr
+				echo "[+] Changing card to Monitor Mode"
+				ifconfig $int down
+				myMAC="00:11:22:33:44:55"
+                		macchanger $int --mac=$myMAC | grep ""
+                		echo "[+] $interface new MAC Address: " $myMAC
+                		sleep 1
+
+				iwconfig $int mode monitor
+				ifconfig $int up
+				echo "[+] Checking for interfering processes:"
+				echo
+		#CHECKING FOR INTERFERING PROCESSES:
+                		sleep 1.5
+                		airmon-ng check $int
+				echo
+				sleep 2
+                #----------------------------------------------------------------------------------
+                #1) Kill dhClient
+                		if [ "$(airmon-ng check wlp2s0 | grep dhclient | gawk '{ FS}  NR==1{print $2}')" == "dhclient" ]; then
+                        		echo "[+] Killing $(airmon-ng check wlp2s0 | grep dhclient | gawk '{ FS}  NR==1{print $2}')..."
+                        		sleep 1
+					kill "$(airmon-ng check wlp2s0 | grep dhclient | gawk '{ FS}  NR==1{print $1}')"
+                        		sleep 5
+                		fi
+		#2) Killing wpa_supplicants
+                		if [ "$(airmon-ng check wlp2s0 | grep wpa_supplicant | gawk '{FS} {print $2}')" == "wpa_supplicant"]; then
+                        		echo "[+] Killing $(airmon-ng check wlp2s0 | grep wpa_supplicant | gawk '{FS} {print $2}')...."
+					sleep 1
+                        		kill "$(airmon-ng check wlp2s0 | grep wpa_supplicant | gawk '{ FS } {print $1}')"
+                        		echo "Please Wait..."
+                        		sleep 5
+                		fi
+		#3) Killing (if Possible, any avahi-daemons
+                		if [ "$(airmon-ng check wlp2s0 | grep avahi-daemon | awk '{FS} NR==1{print $2}')" == "avahi-daemon"]; then
+                        		echo "[+] Killing Avahi-Daemons..."
+                        		kill "$(airmon-ng check wlp2s0 | grep avahi-daemon | awk '{FS} NR==1{print $1}')"
+                        		kill "$(airmon-ng check wlp2s0 | grep avahi-daemon | awk '{FS} NR==2{print $1}')"
+                        		sleep 5
+                		fi
+		#------------------------------------------------------------------------------------
+				sleep 2
+				echo
+                		echo "[+] Checking for interfering processes again"
+                		sleep 1
+                		airmon-ng check $interface
+                		sleep 2
+
+                		echo "[+] Success. Exiting....."
 			;;
 			"N"|"n")
 				sleep 0.4
-				"Exitting..."
-				exit
+				echo "[+] Nothing has been done"
+				echo "Exitting..."
+				break
 			;;
 		esac
 	;;
@@ -37,19 +103,24 @@ case $ans in
 	"managed"|"man")
 		echo "This will set the Network Card to MANAGED Mode"
                 sleep 0.6
-                echo -n "Are you sure you want to continue?[Y/n]: "
+                echo -n "[*] Are you sure you want to continue?[Y/n]: "
                 read yn
+		echo
                 case $yn in
                         "y"|"Y")
                                 ifconfig wlan0 down
                                 iwconfig wlan0 mode managed
                                 ifconfig wlan0 up
-				echo "Restarting Network Manager daemon..."
+				sleep 4
+				echo "[+] Restarting Network Manager daemon..."
 				service NetworkManager restart
-                        ;; 
+                        ;;
                         "N"|"n")
                                 sleep 0.4
-                                "Exitting..."
-                                exit
+                                echo "Exitting..."
+                                break
                         ;;
+		esac
+	;;
 esac
+
